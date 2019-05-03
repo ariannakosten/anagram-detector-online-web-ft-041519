48411b10633dd7e493e20d8dc5fc70f0aed7c866
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-03 09:32:32 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    array.select do |w| 
+       if w.split("").sort == word.split("").sort    #trouble w/ which method comes 1st .split /.sort
+         true
+       else
+                                      #cannot put anything here or it fails ??
+      end
+    end
+  end
+end
