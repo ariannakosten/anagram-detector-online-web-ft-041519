061386351e935ca7f33ea1aa4fae0a52cf5a7fa2
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-05 23:58:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,10 @@ class Anagram
   def match(array)
     array.select do |w| 
        if w.split("").sort == word.split("").sort    #trouble w/ which method comes 1st .split /.sort
+<<<<<<< HEAD
          true
+=======
+>>>>>>> 7db46bf893487609b1692738578ec81e91e5a705
        else
                                       #cannot put anything here or it fails ??
       end
